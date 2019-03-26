5f83234c7bccc5f5357ed1e5d9012a87df5d475d
Mouad Naji <mouadnaji@Mouads-MacBook-Pro.local>
2019-03-26 14:55:22 +0100

    læs mere

--- script.js
+++ script.js
@@ -21,11 +21,11 @@
 
      if (dots.style.display === "none") {
          dots.style.display = "inline";
-         btnText.innerHTML = "Read more";
+         btnText.innerHTML = "Læs mere";
          moreText.style.display = "none";
      } else {
          dots.style.display = "none";
-         btnText.innerHTML = "Read less";
+         btnText.innerHTML = "Læs mindre";
          moreText.style.display = "inline";
      }
  }
